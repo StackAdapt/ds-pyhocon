ffa05513ea53b5faee0bbe539b1bf1d15c3578e6
Bryan Qiang <bryan.qiang@stackadapt.com>
2023-06-27 10:11:11 -0400

    DSI-380 - rename to stackadapt.ds prefix

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 from setuptools.command.test import test as TestCommand
 
 
-NAME = "stackadapt.ds-pyhocon"
+NAME = "stackadapt.ds.pyhocon"
 VERSION = "1.1.1"
 URL = "https://github.com/StackAdapt/ds-pyhocon"
 PACKAGES = find_packages(include=["ds_pyhocon", "ds_pyhocon.*"])
